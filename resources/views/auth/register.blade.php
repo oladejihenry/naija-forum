62a7c30db35026f8920ed58a9a5211ee3c9f04a7
Henry Oladeji <officialmonstajamss@gmail.com>
2020-09-10 22:52:03 +0100

    Minor Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,48 +1,155 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Register - Naijaswift Forum</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" href="{{asset('css/invalid.css')}}">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+            ul {
+                list-style: none;
+                padding: 0;
+            }
+            .inner {
+                    padding-left: 1em;
+                    overflow: hidden;
+                    display: none;
+                
+                }
+            .inner li {
+                font-size: 11px;
+                padding: 5px;
+            }
+            .inner:show {
+                /*display: block;*/
+                }
+            a.toggle {
+                    width: 100%;
+                    display: block;
+                    background: #ffffff;
+                    color: #838383;
+                    padding: 10px 0;
+                    border-radius: 0.15em;
+                    transition: background .3s ease;
+                }
+            a.toggle:hover {
+               
+            }
+        </style>
+    </head>
+<body>
 
-        <x-jet-validation-errors class="mb-4" />
+<header>
+    <a style="color:#7cc341" class="logo" href="/"><h2>Logo</h2></a>
+    <nav>
+        <ul class="nav__links">
+            <li><a href="#">Trending </a></li><p class="chip red">100</p>
+            <li><a href="#">Recent</a></li><p class="chip orange">27</p>
+            <li><a href="#">New</a></li><p class="chip green">40</p>
+        </ul>
+    </nav>
+    <div>
+        <input class="search" type="text" placeholder="Search">
+        <a class="cta2" href="/login">Login</a>
+        <a class="cta" href="/register">Sign up</a>
+    <p class="menu cta">Menu</p>
+    </div>
+    
+</header>
+<div class="overlay">
+    <a class="close">&times;</a>
+    <div class="overlay__content">
+        <a href="#">Trending</a>
+        <a href="#">Recent</a>
+        <a href="#">Login/Signup</a>
+        <input class="search" type="text" placeholder="Search">
+    </div>
+</div>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+<div class="app-content">
+    <div class="login-page">
+        <div class="form">
+            <form method="POST" class="register-form" action="{{ route('register') }}">
+                @csrf
+                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" 
+                value="{{ old('name') }}" placeholder="Full Name" required autocomplete="name" autofocus/>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        <br>
+                    </span>
+                    <br>
+                @enderror
+                <input type="text" name="username" class="form-control @error('name') is-invalid @enderror"
+                value="{{ old('username') }}" placeholder="Username" required autocomplete="username" autofocus/>
+                @error('username')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        <br>
+                    </span>
+                    <br>
+                @enderror
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" 
+                value="{{ old('email') }}" placeholder="Email Address" required autocomplete="email"/>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        <br>
+                    </span>
+                    <br>
+                @enderror
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" 
+                placeholder="Password" required autocomplete="new-password"/>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        <br>
+                    </span>
+                    <br>
+                @enderror
+                <input type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror" 
+                placeholder="Confirm Password" required autocomplete="new-password"/>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        <br>
+                    </span>
+                    <br>
+                @enderror
+                <button>create</button>
+                <p class="message">Already registered? <a href="/login">Sign In</a></p>
+            </form>
 
-            <div>
-                <x-jet-label value="Name" />
-                <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+        </div>
+    </div>
+</div>
+<div class="footer">
+    NAIRALAND 2020
+</div>
+<script type="text/javascript" src="{{asset('js/mobile.js')}}"></script>
+<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5eff351b1692982c192ff95b" type="text/javascript"  crossorigin="anonymous"></script>
 
-            <div class="mt-4">
-                <x-jet-label value="Username" />
-                <x-jet-input class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
-            </div>
+<script>
+    $('.toggle').click(function(e) {
+        e.preventDefault();
+    
+        var $this = $(this);
+    
+        if ($this.next().hasClass('show')) {
+            $this.next().removeClass('show');
+            $this.next().slideUp(350);
+        } else {
+            $this.parent().parent().find('li .inner').removeClass('show');
+            $this.parent().parent().find('li .inner').slideUp(350);
+            $this.next().toggleClass('show');
+            $this.next().slideToggle(350);
+        }
+    });
+</script>
 
-            <div class="mt-4">
-                <x-jet-label value="Email" />
-                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label value="Password" />
-                <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label value="Confirm Password" />
-                <x-jet-input class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</body>
+</html>
