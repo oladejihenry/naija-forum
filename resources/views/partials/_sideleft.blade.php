4c3a356a83a6aa5f4a70a794a40fec4cdf9ca016
Henry Oladeji <officialmonstajamss@gmail.com>
2020-09-11 17:51:57 +0100

    Minor Update

--- resources/views/partials/_sideleft.blade.php
+++ resources/views/partials/_sideleft.blade.php
@@ -2,32 +2,58 @@
     <input style="background:white;width:100%;margin-bottom: 20px;" class="search2" type="text" placeholder="Search">
         <div class="lc-card">
             <div class="lc-card-header">
-                NairaLand Forum
+                Naijaswift Nigerian Forum
             </div>
             <div class="lc-body">
             <ul class="accordion">
                 <li>
                     <a class="toggle" href="javascript:void(0);">General <span style="color:#838383;float:right"><i style="font-size:10px" class="fa fa-chevron-down"></i></span></a>
-                    <ul class="inner">
+                    <ul class="inner" style="display:block;">
                     <li><a href="/politics">Politics</a></li>
-                    <li>Option 2</li>
-                    <li>Option 3</li>
+                    <li><a href="/crime"> Crime </a></li>
+                    <li><a href="/romance"> Romance </a></li>
+                    <li><a href="/jobs"> Jobs/Vacancies </a></li>
+                    <li><a href="/career"> Career </a></li>
+                    <li><a href="/business"> Business </a></li>
+                    <li><a href="/investment"> Investment </a></li>
+                    <li><a href="/nysc"> NYSC </a></li>
+                    <li><a href="/Education"> Education </a></li>
+                    <li><a href="/autos"> Autos </a></li>
+                    <li><a href="/cars"> Car Talk </a></li>
+                    <li><a href="/properties"> Properties </a></li>
+                    <li><a href="/health"> Health </a></li>
+                    <li><a href="/travel"> Travel </a></li>
+                    <li><a href="/family"> Family </a></li>
+                    <li><a href="/culture"> Culture </a></li>
+                    <li><a href="/religion"> Religion </a></li>
+                    <li><a href="/food"> Food </a></li>
+                    <li><a href="/diaries"> Diaries </a></li>
+                    <li><a href="/ads"> Naijaswift Ads </a></li>
+                    <li><a href="/pets"> Pets </a></li>
+                    <li><a href="/agriculture"> Agriculture </a></li>
                     </ul>
                 </li>
                 <li>
                     <a class="toggle" href="javascript:void(0);">Sport  <span style="color:#838383;float:right"><i style="font-size:10px" class="fa fa-chevron-down"></i></span></a>
                     <ul class="inner">
-                        <li>Option 1</li>
-                        <li>Option 2</li>
-                        <li>Option 3</li>
+                        <li><a href="/epl">English Premier League</a></li>
+                        <li><a href="/liga">La Liga</a></li>
+                        <li><a href="/seria">Italian Seria A</a></li>
                     </ul>
                     </li>
                     <li>
                     <a class="toggle" href="javascript:void(0);">Entertainment  <span style="color:#838383;float:right"><i style="font-size:10px" class="fa fa-chevron-down"></i></span></a>
                     <ul class="inner">
-                        <li>Option 1</li>
-                        <li>Option 2</li>
-                        <li>Option 3</li>
+                        <li><a href="/jokes">Jokes</a></li>
+                        <li><a href="/tv">Tv/Movies</a></li>
+                        <li><a href="/music">Music/Radio</a></li>
+                        <li><a href="/celebrities"></a>Celebrities</li>
+                        <li><a href="/fashion">Fashion</a></li>
+                        <li><a href="/events">Events</a></li>
+                        <li><a href="/sports">Sports</a></li>
+                        <li><a href="/gaming">Gaming</a></li>
+                        <li><a href="/forum-game">Forum Game</a></li>
+                        <li><a href="/literature">Literature</a></li>
                     </ul>
                     </li>
                     <li>
